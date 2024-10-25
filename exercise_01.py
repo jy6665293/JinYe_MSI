6b35cd1df8643a596aebd6cb8b777d85d71a6f53
jy6665293 <jy6665293@gmail.com>
2024-10-25 23:15:22 +0200

    JinYe

---
+++ exercise_01.py
@@ -0,0 +1,8 @@
+import numpy as np
+import os
+
+# file_path = 'F:\Freiburg\MSI\exercise\exercise_01\exercise1-aby_msi-main\exercise1-aby_msi-main\exercise1_dataset.npz' 
+data = np.load('F:\Freiburg\MSI\exercise\exercise_01\exercise1-aby_msi-main\exercise1-aby_msi-main\exercise1_dataset.npz')
+#cwd = os.getcwd('exercise1_dataset.npz')
+data.files
+
